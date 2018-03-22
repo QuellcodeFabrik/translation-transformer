b53cc2001e6f06f03be8c84bf8d831312886e23f
Sascha Gros <gros.sascha@gmail.com>
2018-03-22 19:56:15 +0100

    Rename static folder to match Apache setup

--- server.ts
+++ server.ts
@@ -55,8 +55,8 @@ server.use((err: any, req: Request, res: Response, next: NextFunction) => {
   }
 });
 
-// serve static files from static folder
-server.use(express.static('static'));
+// serve static files from dcp folder
+server.use(express.static('dcp'));
 
 // REST test endpoint to check if API is set into place.
 server.get('/api/test', (req: Request, res: Response) => {
